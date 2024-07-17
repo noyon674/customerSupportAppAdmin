d144f78b190f03299ef473548104e0f8fbc54ece
noyon674 <sarker35-674@diu.edu.bd>
2024-07-17 17:22:40 +0600

    complete category form

--- src/views/pages/Category/Category.jsx
+++ src/views/pages/Category/Category.jsx
@@ -22,15 +22,28 @@ function Category() {
             <strong>Category</strong>
           </CCardHeader>
           <CCardBody>
-            <CFormLabel>Name</CFormLabel>
-            <CFormInput
-            type='text'
-            name = 'name'
-            required />
-            <CFormLabel>Description</CFormLabel>
-            <CFormTextarea
-            name='textarea'
-            />
+            <CForm>
+              <CFormLabel>Name</CFormLabel>
+              <CFormInput
+              className='mb-4'
+              type='text'
+              name = 'name'
+              required />
+              <CFormLabel>Description</CFormLabel>
+              <CFormTextarea
+              className='mb-4'
+              name='textarea'
+              rows={6}
+              />
+              <CFormLabel>Thumbnil</CFormLabel>
+              <CFormInput
+              className='mb-4'
+              type='file'
+              />
+              <CButton type="button" class="btn btn-success me-4 text-light">Save</CButton>
+              <CButton type="button" class="btn btn-primary me-4">Save and add another</CButton>
+              <CButton type="button" class="btn btn-secondary">Save and add continue editing</CButton>
+            </CForm>
           </CCardBody>
         </CCard>
       </CCol>
