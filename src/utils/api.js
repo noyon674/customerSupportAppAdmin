9d69359acd484b4791e3304d2a4f19f8527182a9
noyon674 <sarker35-674@diu.edu.bd>
2024-08-14 20:23:17 +0600

    fixed

--- src/utils/api.js
+++ src/utils/api.js
@@ -17,7 +17,6 @@ const fetchData = async (method, url, data = null) => {
 // Articles API
 export const getArticles = () => fetchData('get', '/articles/');
 export const addArticle = (userdata) => fetchData('post', '/articles/', userdata);
-<<<<<<< HEAD
 export const deleteArticle = (id) => fetchData('delete', `/articles/${id}/`);
 
 // FAQs API
