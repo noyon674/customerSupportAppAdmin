2ae6cc15a595843734a539aaf6ce634db96bfcd2
noyon674 <developernoyon9@gmail.com>
2024-09-19 15:06:29 +0600

    added logger middleware

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
-import { legacy_createStore as createStore } from 'redux'
+import { legacy_createStore as createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 import reducer from './redux/reducer'
 
 
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(logger))
 export default store
